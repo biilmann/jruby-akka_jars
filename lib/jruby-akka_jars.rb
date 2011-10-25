fa296088dab5b7a7c9dc10bccaa0c92de47a4811
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2011-10-25 14:22:16 +0200

    require all jars

--- lib/jruby-akka_jars.rb
+++ lib/jruby-akka_jars.rb
@@ -1,7 +1,7 @@
+require "java"
 require "jruby-akka_jars/version"
+require "scala-library.jar"
 
-module Jruby
-  module AkkaJars
-    # Your code goes here...
-  end
+Dir[File.join(File.dirname(__FILE__), "akka", "*.jar")].each do |jar|
+  require jar
 end
